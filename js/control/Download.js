63ad4070ca8ef032fda4eac77e0b32840ef93462
Rodolphe PELLOUX-PRAYER <rodolphe@damsy.net>
2016-08-05 09:43:30 +0200

    Fix download links

--- js/control/Download.js
+++ js/control/Download.js
@@ -2,8 +2,8 @@ BR.Download = L.Class.extend({
     update: function (urls) {
         if (urls) {
             ['gpx', 'kml', 'geojson', 'csv'].forEach(function(e, i, a) {
-                var a = L.DomUtil.get('dl-gpx');
-                a.setAttribute('href', urls.gpx);
+                var a = L.DomUtil.get('dl-'+e);
+                a.setAttribute('href', urls[e]);
                 a.setAttribute('download', 'brouter.'+e);
                 a.removeAttribute('disabled');
             })
